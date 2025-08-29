8ec86bf77dec7c1744d2d49f0fbd33bd424cf96d
vyakateshchavan3333 <chavanvyakatesh3333@gmail.com>
2025-08-29 18:06:29 +0530

    Selection Sort

---
+++ selection_sort.js
@@ -0,0 +1,15 @@
+// Selection Sort
+let arr=[23,34,75,92,1,2,4,6,74,555];
+for(let i=0;i<arr.length-1;i++){
+    let current_min = i;
+    for(let j=i+1;j<arr.length; j++){
+        if(arr[j]<arr[current_min]){
+            current_min =j;
+        }
+        
+    }
+    let temp = arr[current_min];
+        arr[current_min] =arr[i];
+    arr[i]=temp
+}
+console.log(arr)
